7b804b509444c86c238d8955e4173824ef050399
Ehab <ehab@Ehabs-MacBook-Pro.local>
2021-03-15 19:54:40 +0100

    bulid exersese and project then .deploy in netlify

--- week1/homework/src/App.js
+++ week1/homework/src/App.js
@@ -6,8 +6,11 @@ import "./App.css";
 function App() {
   return (
     <div>
+      <h1 className="header">Hobby List</h1>
       <HobbyList />
+      <h1 className="header">Guarantee</h1>
       <Guarantee />
+      <h1 className="header">Counter</h1>
       <Counter />
     </div>
   );
