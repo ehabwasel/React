27b8e91d5cac994c04d85a6339280fb9aac09f30
Ehab <ehab@Ehabs-MacBook-Pro.local>
2021-03-29 20:26:02 +0200

    Weather project

---
+++ week3/react_weather/src/Component/Card.js
@@ -0,0 +1,54 @@
+import React from "react";
+import uuid from "react-uuid";
+import { FaTimes } from "react-icons/fa";
+
+const Card = ({ weatherData, setWeatherData, setCityName }) => {
+  const remove = (id) => {
+    setWeatherData(weatherData.filter((data) => data.id !== id));
+  };
+  const Card_item = ({ city }) => {
+    return (
+      <div className="city_card">
+        <FaTimes
+          style={{ color: "red", cursor: "pointer", float: "right" }}
+          onClick={() => remove(city.id)}
+        />
+
+        <h3>
+          {`${city.name} 
+          ${city.sys.country}`}
+        </h3>
+        <p>{city.weather[0].main}</p>
+        <div className="info">
+          <h5>{city.weather[0].description}</h5>
+          <img
+            className="city-icon"
+            src={`https://openweathermap.org/img/wn/${city.weather[0].icon}@2x.png`}
+            alt={city.weather[0].description}
+          />
+
+          <span></span>
+          <p>
+            {`Min Temp : ${Math.round(city.main.temp_min)}`}
+            <sup>&deg;C</sup>
+          </p>
+          <p>
+            {` Max Temp : ${city.main.temp_max}`}
+            <sup>&deg;C</sup>
+          </p>
+          <p>{`Location : ${city.coord.lat} , ${city.coord.lon}`}</p>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <>
+      {weatherData.map((city) => (
+        <Card_item key={uuid()} city={city} remove={remove} />
+      ))}
+    </>
+  );
+};
+
+export default Card;
---
+++ week3/react_weather/src/Component/Fetch.js
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react";
+import Button from "./Button";
+import Card from "./Card";
+
+const API_KEY = process.env.REACT_APP_OPENWEATHERMAP_API_KEY;
+
+const Fetch = ({ cityName, setCityName }) => {
+  const [weatherData, setWeatherData] = useState([]);
+  const [hasError, setHasError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const getCity = async () => {
+    try {
+      setLoading(true);
+      const res = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${API_KEY}`
+      );
+
+      const data = await res.json();
+      if (data.cod == 200) {
+        setHasError(false);
+        setWeatherData([data, ...weatherData]);
+        setLoading(false);
+        setCityName("");
+      } else {
+        setHasError(true);
+        setLoading(false);
+      }
+    } catch (err) {
+      setHasError(true);
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div>
+      <Button onClickEvent={getCity} disabled={!cityName} />
+      {!hasError && loading && <p>LOADIN ....</p>}
+      {!hasError && weatherData && (
+        <Card weatherData={weatherData} setWeatherData={setWeatherData} cityName={{ cityName }} />
+      )}
+      {hasError && <p>OOPS!!..Failed to get data or write correct City Name ....</p>}
+    </div>
+  );
+};
+
+export default Fetch;
